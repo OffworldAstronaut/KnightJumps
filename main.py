ab585e6e2fdc00a74ea5d404312f48686fdc79c2
OffworldAstronaut <rafael.science.amorim@gmail.com>
2025-09-03 18:38:37 -0300

    feat: revamp -- implementing warnsdorf's rule instead of random walk approach

--- main.py
+++ main.py
@@ -1,41 +1,41 @@
-# OBJECTIVE: simulate many knights moving through a chessboard until it visits all 
-# sites of the board. 
+# OBJECTIVE: Implement a Warnsdorf's Rule algorithm to find a Knight Tour.
 
-# imports 
-
+# Modules 
 import numpy as np
 
-# functions 
+# Classes 
+class Knight:
+    def __init__(self, start_row, start_col):
+        self.row = start_row
+        self.col = start_col
+        self.move_offsets = [
+            (2, 1), (2, -1), (-2, 1), (-2, -1),
+            (1, 2), (1, -2), (-1, 2), (-1, -2)
+        ]
+        
+    def get_possible_moves(self):
+        moves = []
+        for r_offset, c_offset in self.move_offsets:
+            new_row = self.row + r_offset
+            new_col = self.col + c_offset
+            moves.append((new_row, new_col))
+        return moves
 
-def move_knight():
-    moves = [np.array([-2, 1]),
-             np.array([-1, 2]),
-             np.array([1, 2]),
-             np.array([2, 1]),
-             np.array([2, -1]),
-             np.array([1, -2]), 
-             np.array([-1. -2]),
-             np.array([-2, -1])]
-    
-    available_moves = []
-            
-    for move in moves:
-        if knight[0] + move[0] < 7 and knight[0] + move[0] > 0:
-            available_moves.append(move)
-        if knight[1] + move[1] < 7 and knight[1] + move[1] > 0:
-            available_moves.append(move)
-            
-    chosen_move = np.random.choice(available_moves, 1)
-    
-    return chosen_move
-
-    
-# main code
-
-# the matrix represents a normal board, 8x8
-board = np.zeros(shape=(8, 8), dtype=bool)
-
-# represents a knight with coordinates (row, column)
-# assigns it to a random spot
-knight = np.array([np.random.randint(0, 7), np.random.randint(0, 7)])
-
+class Board:
+    def __init__(self, start_row, start_col):
+        self.warnsdorf_board = np.zeros(shape=(8, 8), dtype=int)
+        self.already_visited = np.zeros(shape=(8, 8), dtype=bool)
+        self.knight = Knight(start_row, start_col)
+        self.visited_count = 1 
+        self.already_visited[start_row, start_col] = True
+        self.path = [(start_row, start_col)]
+        
+    def is_valid_move():
+        ...
+        
+    def get_accessibility():
+        ...
+        
+    # Carries on with the simulation, moving the knight according to the rule
+    def run_tour():
+        ...
